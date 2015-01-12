c0c75aa8271534500fbccc77ec7757748678dd2c
Leenix <leenix48@gmail.com>
2015-01-12 10:12:57 +1000

    Initial Commit
    Ported project files into directory

---
+++ thingspeak_settings.py
@@ -0,0 +1,16 @@
+__author__ = 'Leenix'
+
+KEY_MAP = {
+    "air_temp": "field1",
+    "wall_temp": "field2",
+    "surface_temp": "field3",
+    "case_temp": "field4",
+    "humidity": "field5",
+    "illuminance": "field6",
+    "sound": "field7",
+    "battery": "field8"
+}
+
+CHANNEL_MAP = {
+    "<id_here>": "<API_KEY_HERE",
+}
